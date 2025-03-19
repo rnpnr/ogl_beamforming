e077636c5693766cbde64fa20117534a6b3f1014
Randy Palamar <randy@rnpnr.xyz>
2025-03-19 07:49:32 -0600

    add comp_paper program
    this will get moved to a different repo eventually but i'm putting
    in this branch here for now

---
+++ helpers/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+base=/tmp/downloads
+output=${base}
+
+#studies="
+#	250317_MN45-1_3.30MHz_ATS539_Cyst_TPW-128-TxRow
+#	250317_MN45-1_3.30MHz_ATS539_Cyst_TPW-128-TxColumn
+#	250317_MN45-1_3.30MHz_ATS539_Cyst_VLS-128-TxRow
+#	250317_MN45-1_3.30MHz_ATS539_Cyst_VLS-128-TxColumn
+#"
+
+#axial="{5e-3, 120e-3}"
+#lateral="{-30e-3, 30e-3}"
+#
+#while (true); do
+#	for study in ${studies}; do
+#		./helper --axial "${axial}" --lateral "${lateral}" ${study} ${base} ${base}/${study}*.zst
+#		echo "showing: ${study}"
+#		read _
+#	done
+#done
+
+#./comp_paper --hilbert --export ${output} ${base}
+./comp_paper --hilbert ${base}
+#./comp_paper ${base}
