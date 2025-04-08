a229509547c0300afce2b9c4e7da5235148a5333
Randy Palamar <randy@rnpnr.xyz>
2025-04-08 05:56:24 -0600

    add throughput measurement tool
    home desktop new method: 32-Frame Average:  183.329 [ms] |    1.278 GB/s
    home desktop old method: 32-Frame Average:  225.283 [ms] |    1.040 GB/s

--- helpers/build.sh
+++ helpers/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-clang -march=native -O2 send_ornot.c ogl_beamformer_lib.c -o helper -lzstd
+#clang -march=native -O2 send_ornot.c ogl_beamformer_lib.c -o helper -lzstd
+#clang -c send_ornot.c -fms-extensions -Wno-ignored-attributes -o /dev/null
 
 #clang -march=native -O3 -fsanitize=address,undefined comp_paper.c ogl_beamformer_lib.c -o comp_paper -lzstd -lfftw3f -lpthread
-clang -march=native -O3 comp_paper.c ogl_beamformer_lib.c -o comp_paper -lzstd -lfftw3f -lpthread
+#clang -march=native -O3 comp_paper.c ogl_beamformer_lib.c -o comp_paper -lzstd -lfftw3f -lpthread
 #clang -march=native -O0 -ggdb comp_paper.c ogl_beamformer_lib.c -o comp_paper -lzstd -lfftw3f
 
-#clang -c send_ornot.c -fms-extensions -Wno-ignored-attributes -o /dev/null
+clang -march=native -O3 throughput.c -o throughput -lzstd
