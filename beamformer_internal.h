7695cb502927fba6f43d4e684abfac6d0f010778
Randy Palamar <randy@rnpnr.xyz>
2026-01-13 12:31:00 -0700

    vulkan: use DEBUG_IMPORT for symbol import into dll on w32

--- beamformer_internal.h
+++ beamformer_internal.h
@@ -59,19 +59,19 @@ typedef struct {
 
 ///////////////////////////
 // NOTE: vulkan layer API
-DEBUG_EXPORT void vk_load(OSLibrary vulkan, Arena *memory, Stream *error);
+DEBUG_IMPORT void vk_load(OSLibrary vulkan, Arena *memory, Stream *error);
 
-DEBUG_EXPORT GPUInfo *vk_gpu_info(void);
+DEBUG_IMPORT GPUInfo *vk_gpu_info(void);
 
-DEBUG_EXPORT void vk_buffer_allocate(GPUBuffer *, iz size, GPUBufferCreateFlags flags, OSHandle *export, s8 label);
-DEBUG_EXPORT void vk_buffer_release(GPUBuffer *);
-DEBUG_EXPORT void vk_buffer_range_upload(GPUBuffer *, void *data, u64 offset, u64 size, b32 non_temporal);
-DEBUG_EXPORT u64  vk_round_up_to_sync_size(u64, u64 min);
+DEBUG_IMPORT void vk_buffer_allocate(GPUBuffer *, iz size, GPUBufferCreateFlags flags, OSHandle *export, s8 label);
+DEBUG_IMPORT void vk_buffer_release(GPUBuffer *);
+DEBUG_IMPORT void vk_buffer_range_upload(GPUBuffer *, void *data, u64 offset, u64 size, b32 non_temporal);
+DEBUG_IMPORT u64  vk_round_up_to_sync_size(u64, u64 min);
 
 // NOTE: temporary API
-DEBUG_EXPORT b32 vk_buffer_needs_sync(GPUBuffer *);
+DEBUG_IMPORT b32 vk_buffer_needs_sync(GPUBuffer *);
 
-DEBUG_EXPORT VulkanHandle vk_semaphore_create(OSHandle *export);
+DEBUG_IMPORT VulkanHandle vk_semaphore_create(OSHandle *export);
 
 ///////////////////////////////
 // NOTE: CUDA Library Bindings
