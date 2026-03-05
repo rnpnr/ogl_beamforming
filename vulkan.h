68fbcf7f9e0cd0f98b9b0f1a7070381e6184c79a
Randy Palamar <randy@rnpnr.xyz>
2026-03-05 09:07:44 -0700

    vulkan: add object labeling in debug builds

--- vulkan.h
+++ vulkan.h
@@ -122,6 +122,7 @@ typedef enum {
 	VK_STRUCTURE_TYPE_SEMAPHORE_GET_WIN32_HANDLE_INFO_KHR                              = 1000078003,
 	VK_STRUCTURE_TYPE_SEMAPHORE_GET_FD_INFO_KHR                                        = 1000079001,
 	VK_STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO                                   = 1000127001,
+	VK_STRUCTURE_TYPE_DEBUG_UTILS_OBJECT_NAME_INFO_EXT                                 = 1000128000,
 	VK_STRUCTURE_TYPE_SEMAPHORE_TYPE_CREATE_INFO                                       = 1000207002,
 	VK_STRUCTURE_TYPE_TIMELINE_SEMAPHORE_SUBMIT_INFO                                   = 1000207003,
 	VK_STRUCTURE_TYPE_SEMAPHORE_WAIT_INFO                                              = 1000207004,
@@ -140,6 +141,68 @@ typedef enum {
 	VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SHADER_RELAXED_EXTENDED_INSTRUCTION_FEATURES_KHR = 1000558000,
 	VK_STRUCTURE_TYPE_MAX_ENUM                                                         = 0x7FFFFFFF,
 } VkStructureType;
+
+typedef enum {
+	VK_OBJECT_TYPE_UNKNOWN                         = 0,
+	VK_OBJECT_TYPE_INSTANCE                        = 1,
+	VK_OBJECT_TYPE_PHYSICAL_DEVICE                 = 2,
+	VK_OBJECT_TYPE_DEVICE                          = 3,
+	VK_OBJECT_TYPE_QUEUE                           = 4,
+	VK_OBJECT_TYPE_SEMAPHORE                       = 5,
+	VK_OBJECT_TYPE_COMMAND_BUFFER                  = 6,
+	VK_OBJECT_TYPE_FENCE                           = 7,
+	VK_OBJECT_TYPE_DEVICE_MEMORY                   = 8,
+	VK_OBJECT_TYPE_BUFFER                          = 9,
+	VK_OBJECT_TYPE_IMAGE                           = 10,
+	VK_OBJECT_TYPE_EVENT                           = 11,
+	VK_OBJECT_TYPE_QUERY_POOL                      = 12,
+	VK_OBJECT_TYPE_BUFFER_VIEW                     = 13,
+	VK_OBJECT_TYPE_IMAGE_VIEW                      = 14,
+	VK_OBJECT_TYPE_SHADER_MODULE                   = 15,
+	VK_OBJECT_TYPE_PIPELINE_CACHE                  = 16,
+	VK_OBJECT_TYPE_PIPELINE_LAYOUT                 = 17,
+	VK_OBJECT_TYPE_RENDER_PASS                     = 18,
+	VK_OBJECT_TYPE_PIPELINE                        = 19,
+	VK_OBJECT_TYPE_DESCRIPTOR_SET_LAYOUT           = 20,
+	VK_OBJECT_TYPE_SAMPLER                         = 21,
+	VK_OBJECT_TYPE_DESCRIPTOR_POOL                 = 22,
+	VK_OBJECT_TYPE_DESCRIPTOR_SET                  = 23,
+	VK_OBJECT_TYPE_FRAMEBUFFER                     = 24,
+	VK_OBJECT_TYPE_COMMAND_POOL                    = 25,
+	VK_OBJECT_TYPE_DESCRIPTOR_UPDATE_TEMPLATE      = 1000085000,
+	VK_OBJECT_TYPE_SAMPLER_YCBCR_CONVERSION        = 1000156000,
+	VK_OBJECT_TYPE_PRIVATE_DATA_SLOT               = 1000295000,
+	VK_OBJECT_TYPE_SURFACE_KHR                     = 1000000000,
+	VK_OBJECT_TYPE_SWAPCHAIN_KHR                   = 1000001000,
+	VK_OBJECT_TYPE_DISPLAY_KHR                     = 1000002000,
+	VK_OBJECT_TYPE_DISPLAY_MODE_KHR                = 1000002001,
+	VK_OBJECT_TYPE_DEBUG_REPORT_CALLBACK_EXT       = 1000011000,
+	VK_OBJECT_TYPE_VIDEO_SESSION_KHR               = 1000023000,
+	VK_OBJECT_TYPE_VIDEO_SESSION_PARAMETERS_KHR    = 1000023001,
+	VK_OBJECT_TYPE_CU_MODULE_NVX                   = 1000029000,
+	VK_OBJECT_TYPE_CU_FUNCTION_NVX                 = 1000029001,
+	VK_OBJECT_TYPE_DEBUG_UTILS_MESSENGER_EXT       = 1000128000,
+	VK_OBJECT_TYPE_ACCELERATION_STRUCTURE_KHR      = 1000150000,
+	VK_OBJECT_TYPE_VALIDATION_CACHE_EXT            = 1000160000,
+	VK_OBJECT_TYPE_ACCELERATION_STRUCTURE_NV       = 1000165000,
+	VK_OBJECT_TYPE_PERFORMANCE_CONFIGURATION_INTEL = 1000210000,
+	VK_OBJECT_TYPE_DEFERRED_OPERATION_KHR          = 1000268000,
+	VK_OBJECT_TYPE_INDIRECT_COMMANDS_LAYOUT_NV     = 1000277000,
+	VK_OBJECT_TYPE_CUDA_MODULE_NV                  = 1000307000,
+	VK_OBJECT_TYPE_CUDA_FUNCTION_NV                = 1000307001,
+	VK_OBJECT_TYPE_BUFFER_COLLECTION_FUCHSIA       = 1000366000,
+	VK_OBJECT_TYPE_MICROMAP_EXT                    = 1000396000,
+	VK_OBJECT_TYPE_TENSOR_ARM                      = 1000460000,
+	VK_OBJECT_TYPE_TENSOR_VIEW_ARM                 = 1000460001,
+	VK_OBJECT_TYPE_OPTICAL_FLOW_SESSION_NV         = 1000464000,
+	VK_OBJECT_TYPE_SHADER_EXT                      = 1000482000,
+	VK_OBJECT_TYPE_PIPELINE_BINARY_KHR             = 1000483000,
+	VK_OBJECT_TYPE_DATA_GRAPH_PIPELINE_SESSION_ARM = 1000507000,
+	VK_OBJECT_TYPE_EXTERNAL_COMPUTE_QUEUE_NV       = 1000556000,
+	VK_OBJECT_TYPE_INDIRECT_COMMANDS_LAYOUT_EXT    = 1000572000,
+	VK_OBJECT_TYPE_INDIRECT_EXECUTION_SET_EXT      = 1000572001,
+	VK_OBJECT_TYPE_MAX_ENUM                        = 0x7FFFFFFF
+} VkObjectType;
 
 typedef enum {
 	VK_PHYSICAL_DEVICE_TYPE_OTHER          = 0,
@@ -2935,6 +2998,13 @@ typedef struct {
 	const VkValidationFeatureDisableEXT * pDisabledValidationFeatures;
 } VkValidationFeaturesEXT;
 
+typedef struct {
+	VkStructureType sType;
+	const void *    pNext;
+	VkObjectType    objectType;
+	uint64_t        objectHandle;
+	const char *    pObjectName;
+} VkDebugUtilsObjectNameInfoEXT;
 
 /* X(name, ret, params) */
 #define VkLoaderProcedureList \
@@ -2942,7 +3012,9 @@ typedef struct {
 
 /* X(name, ret, params) */
 #define VkBaseProcedureList \
-	X(vkCreateInstance, VkResult, (const VkInstanceCreateInfo *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkInstance *pInstance)) \
+	X(vkCreateInstance,                       VkResult, (const VkInstanceCreateInfo *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkInstance *pInstance)) \
+	X(vkEnumerateInstanceExtensionProperties, VkResult, (const char *pLayerName, uint32_t *pPropertyCount, VkExtensionProperties *pProperties)) \
+	X(vkEnumerateInstanceLayerProperties,     VkResult, (uint32_t *pPropertyCount, VkLayerProperties *pProperties)) \
 
 /* X(name, ret, params) */
 #define VkInstanceProcedureList \
@@ -2995,6 +3067,7 @@ typedef struct {
 	X(vkGetSemaphoreWin32HandleKHR,    VkResult, (VkDevice device, const VkSemaphoreGetWin32HandleInfoKHR *pGetWin32HandleInfo, void **pHandle)) \
 	X(vkInvalidateMappedMemoryRanges,  VkResult, (VkDevice device, uint32_t memoryRangeCount, const VkMappedMemoryRange *pMemoryRanges)) \
 	X(vkMapMemory,                     VkResult, (VkDevice device, VkDeviceMemory memory, VkDeviceSize offset, VkDeviceSize size, VkMemoryMapFlags flags, void **ppData)) \
+	X(vkSetDebugUtilsObjectNameEXT,    VkResult, (VkDevice device, const VkDebugUtilsObjectNameInfoEXT *pNameInfo)) \
 	X(vkSignalSemaphore,               VkResult, (VkDevice device, const VkSemaphoreSignalInfo *pSignalInfo)) \
 	X(vkUnmapMemory,                   void,     (VkDevice device, VkDeviceMemory memory)) \
 	X(vkUpdateDescriptorSets,          void,     (VkDevice device, uint32_t descriptorWriteCount, const VkWriteDescriptorSet *pDescriptorWrites, uint32_t descriptorCopyCount, const VkCopyDescriptorSet *pDescriptorCopies)) \
