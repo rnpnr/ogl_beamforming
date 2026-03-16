34407829671aa5049fc9b98b6f99b50b2e09f1dc
Randy Palamar <randy@rnpnr.xyz>
2026-03-16 09:51:08 -0600

    shaders/coherency_weighting: drop unused "elements" push constant

--- generated/beamformer.meta.c
+++ generated/beamformer.meta.c
@@ -232,7 +232,6 @@ typedef struct {
 typedef struct {
 	u64 left_side_buffer;
 	u64 right_side_buffer;
-	u32 elements;
 	f32 scale;
 	u32 output_size_x;
 	u32 output_size_y;
@@ -698,7 +697,6 @@ read_only global s8 beamformer_shader_global_header_strings[] = {
 	"layout(push_constant, std430) uniform PushConstants {\n"
 	"  uint64_t  left_side_buffer;\n"
 	"  uint64_t  right_side_buffer;\n"
-	"  uint32_t  elements;\n"
 	"  float32_t scale;\n"
 	"  uint32_t  output_size_x;\n"
 	"  uint32_t  output_size_y;\n"
